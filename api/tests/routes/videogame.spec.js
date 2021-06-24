864f42cd6e66db487514b233d4bd734d5f612fa4
Gustavitory <wereberfan97@gmail.com>
2021-06-24 08:08:01 -0400

    todo

--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -22,3 +22,54 @@ describe('Videogame routes', () => {
     );
   });
 });
+describe('POST /videogames', () => {
+  it('responds with 200', () => agent.post('/pokemons').expect(200))
+  it('should return the detail of the game', () => 
+    agent.post('/videogames/add')
+    .send({
+      name:"azul",
+      description:"caza monstruos con un arma innecesariamente adornada y con un gatillo carente de sentido",
+      rating:0.0,
+      platforms:[{"play":true},{"Xbox":true},{"PC":false}],
+      genres:["1","2"],
+      released:"14/01/1997"
+  })
+      .then(function(res){
+        expect(res.body).to.deep.equal({
+          id: 700001,
+          name: "azul",
+          description: "caza monstruos con un arma innecesariamente adornada y con un gatillo carente de sentido",
+          released: "14/01/1997",
+          rating: 0,
+          platforms: "play, Xbox, PC.",
+          background_image: "https://media.giphy.com/media/KluRpaqe0ZyyFRfDzi/giphy.gif",
+          createdAt: "2021-06-24T04:06:14.505Z",
+          updatedAt: "2021-06-24T04:06:14.505Z",
+          genres: [
+              {
+                  id: 1,
+                  name: "Action",
+                  createdAt: "2021-06-24T04:05:48.953Z",
+                  updatedAt: "2021-06-24T04:05:48.953Z",
+                  game_gen: {
+                      createdAt: "2021-06-24T04:06:14.754Z",
+                      updatedAt: "2021-06-24T04:06:14.754Z",
+                      videogameId: 700001,
+                      genreId: 1
+                  }
+              },
+              {
+                  id: 2,
+                  name: "Indie",
+                  createdAt: "2021-06-24T04:05:48.955Z",
+                  updatedAt: "2021-06-24T04:05:48.955Z",
+                  game_gen: {
+                      createdAt: "2021-06-24T04:06:14.766Z",
+                      updatedAt: "2021-06-24T04:06:14.766Z",
+                      videogameId: 700001,
+                      genreId: 2
+                  }
+              }
+          ]
+      })
+}))});
